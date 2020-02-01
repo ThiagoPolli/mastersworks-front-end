63c0f3c78732c9cc184006d81a73126cd1dbda8b
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-02-01 00:31:11 -0300

    FormGroup para controle de formulario de cadastro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -49,6 +49,8 @@ export class HomePage {
    
    
   }
- 
+  signup() {
+    this.navCtrl.push('SignupPage');
+  }
  
 }
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder ) {
+
+      this.formGroup = this.formBuilder.group({
+        nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+        email: ['', [Validators.required, Validators.email]],
+        tipo : ['', [Validators.required]],
+        cpfOuCnpj : ['', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        senha : ['', [Validators.required]],
+        logradouro : ['', [Validators.required]],
+        numero : ['', [Validators.required]],
+        complemento : ['', []],
+        bairro : ['', []],
+        cep : ['', [Validators.required]],
+        telefone1 : ['', [Validators.required]],
+        telefone2 : ['', []],
+        telefone3 : ['', []],
+        estadoId : [null, [Validators.required]],
+        cidadeId : [null, [Validators.required]]      
+      });
+  }
+  signupUser(){
+    console.log("enviou o Form");
+  }
+ 
+}
