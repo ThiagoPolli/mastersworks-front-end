c9d72bfe656ad06a38320e85fe6bd0897feb0116
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-02-02 18:15:19 -0300

    botao flutuante e imagem carrinho vazio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class MyApp {
      
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      {title: 'Carrinho', component: 'CartPage' },
       {title: 'Logout', component: '' }
     ];
 
@@ -52,4 +53,5 @@ export class MyApp {
 
   }  
  }
+
 }
